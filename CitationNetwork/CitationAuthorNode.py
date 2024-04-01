00fac01667c659546797e63290fdf2f127230986
guopei <376980440@qq.com>
2024-04-01 09:37:09 +0800

    proj2

--- CitationNetwork/CitationAuthorNode.py
+++ CitationNetwork/CitationAuthorNode.py
@@ -2,7 +2,13 @@ from Framework.GraphDataNodeBase import GraphDataNodeBase
 
 
 class CitationAuthorNode(GraphDataNodeBase):
-    def __init__(self, data_type: str) -> None:
-        super().__init__(data_type)
+    def __init__(self) -> None:
+        super().__init__('CitationAuthorNode')
+        self.ID = 0
+
+    def SetAuthorID(self, id):
+        self.ID = id
+
+
         
     
