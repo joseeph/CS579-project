a4bc5f4a26428a19867e572525704113e26765c9
Zhouyang Wang <wangzhouy@foxmail.com>
2024-04-27 20:29:12 +0800

    add closeness

--- StandfordCitationNetworkAnalysis/CoauthorNetworkBuilder.py
+++ StandfordCitationNetworkAnalysis/CoauthorNetworkBuilder.py
@@ -12,6 +12,14 @@ class CoauthorNetworkBuilder:
     def __init__(self) -> None:
         pass
 
+    def find_max_cen(self, group):
+        max_cen = group['DegreeCentrality'].max()
+        max_score = group['ClosenessCentrality'].max()
+        selected_rows =  group[(group['DegreeCentrality'] == max_cen) & (group['ClosenessCentrality'] == max_score)]
+        return selected_rows.assign(
+        Degree_Centrality_sum=group['DegreeCentrality'].sum(),
+        Closeness_Centrality_sum=group['ClosenessCentrality'].sum())
+
     def BuildCoauthorDataFrame(self, data_container :NodeContainer, output_path):
         paper_map = data_container.GetAllNodesByType("CitationPaperNode")
 
@@ -33,7 +41,7 @@ class CoauthorNetworkBuilder:
                     if first_author in paper_name_dict:
                         paper_name_dict[first_author]['paper'].append(paper_node.Name)
                     else:
-                        paper_name_dict[first_author] = {'paper': [paper_node.Name], 'author':'first'}
+                        paper_name_dict[first_author] = {'paper': [paper_node.Name], 'author':'first', 'paper_id':paper_id}
                     if len(paper_node.AuthorNames)>1:
                         for coauthor_name in paper_node.AuthorNames[1:]:
                             # citation_paper :CitationPaperNode = data_container.FindNodeWithType("CitationPaperNode", citation_paperid)
@@ -43,18 +51,18 @@ class CoauthorNetworkBuilder:
                             if coauthor_name in paper_name_dict:
                                 paper_name_dict[coauthor_name]['paper'].append(paper_node.Name)
                             else:
-                                paper_name_dict[coauthor_name] = {'paper': [paper_node.Name], 'author':'second'}
+                                paper_name_dict[coauthor_name] = {'paper': [paper_node.Name], 'author':'second', 'paper_id':paper_id}
             nx.set_node_attributes(coauthor_graph, paper_name_dict)
             graph_dict[cur_year] = coauthor_graph
         # degree centrality
-        in_degree_centrality_map = {}
-        out_degree_centrality_map = {}
+        degree_centrality_map = {}
+        closeness_centrality_map = {}
         for cur_year in range(from_year, to_year + 1):
             graph = graph_dict[cur_year]
-            in_degree_map = nx.in_degree_centrality(graph)
-            out_degree_map = nx.out_degree_centrality(graph)
-            in_degree_centrality_map[cur_year] = in_degree_map
-            out_degree_centrality_map[cur_year] = out_degree_map
+            degree_map = nx.degree_centrality(graph)
+            close_degree_map = nx.closeness_centrality(graph)
+            degree_centrality_map[cur_year] = degree_map
+            closeness_centrality_map[cur_year] = close_degree_map
 
         # betweenness centrality
         
@@ -68,37 +76,44 @@ class CoauthorNetworkBuilder:
         df = pd.DataFrame()
         nodeid_list = []
         year_list = []
-        in_degreecen_list = []
-        out_degreecen_list = []
+        degreecen_list = []
+        closeness_list = []
         paper_name_list = []
         authorship= []
-        betweenness_centrality_list = []
+
+        paper_id_list = []
         for cur_year in range(from_year, to_year + 1):
             cur_graph :nx.DiGraph = graph_dict[cur_year]
-            in_degree_cen = in_degree_centrality_map[cur_year]
-            out_degree_cen = out_degree_centrality_map[cur_year]
-
+            degree_cen = degree_centrality_map[cur_year]
+            closeness_cen = closeness_centrality_map[cur_year]
             for node_id, attr in cur_graph.nodes(data=True):
                 # node id
                 nodeid_list.append(node_id)
                 year_list.append(cur_year)
-                in_degree_centrality = in_degree_cen[node_id]
-                out_degree_centrality = out_degree_cen[node_id]
-                in_degreecen_list.append(in_degree_centrality)
-                out_degreecen_list.append(out_degree_centrality)
+                degree_centrality = degree_cen[node_id]
+                close_centrality = closeness_cen[node_id]
+                degreecen_list.append(degree_centrality)
+                closeness_list.append(close_centrality)
                 print(attr)
                 paper_name_list.append(attr['paper'])
                 authorship.append(attr['author'])
+                paper_id_list.append(attr['paper_id'])
             
-        df['NodeID'] = nodeid_list
+        df['NodeID'] = paper_id_list
+
         df['Year'] = year_list
-        df['InDegreeCentrality'] = in_degreecen_list
-        df['OutDegreeCentrality'] = out_degreecen_list
-        df['paper'] = paper_name_list
-        df['authorship'] = authorship
+        df['DegreeCentrality'] = degreecen_list
+        df['ClosenessCentrality'] = closeness_list
+
         
-        UtilFuncs.PickleWrite(df, output_path)
-        
+        grouped = df.groupby(['NodeID', 'Year'])
+        # Apply the function to each group
+        highest_df = grouped.apply(self.find_max_cen)
+
+        # Reset index to obtain a flat DataFrame
+        highest_df = highest_df.reset_index(drop=True)
+
+        return highest_df
 
         #pd.DataFrame()
         pass 
